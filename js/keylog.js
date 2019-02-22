407307d1b273085f2a12208c684af05319ec221e
dandeeccastro <dundeepessoal@gmail.com>
2019-02-22 13:54:31 -0300

    Failed attempt of system communication w pynput

--- js/keylog.js
+++ js/keylog.js
@@ -1,3 +1,4 @@
+// Essa função detecta keypresses no documento. Como fazer ela funcionar para o sistema todo?
 document.onkeypress = function(e) {
     e = e || window.event;
     var charCode = (typeof e.which == "number") ? e.which : e.keyCode;
