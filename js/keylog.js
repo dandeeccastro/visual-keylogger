9361270520c6ebb04fba881f523271dd5b0bf8e9
dandeeccastro <dundeepessoal@gmail.com>
2019-02-23 14:06:19 -0300

    Functional key detection w python-shell and javascript

--- js/keylog.js
+++ js/keylog.js
@@ -1,8 +1,31 @@
-// Essa função detecta keypresses no documento. Como fazer ela funcionar para o sistema todo?
+/* Essa função detecta keypresses no documento. Como fazer ela funcionar para o sistema todo?
 document.onkeypress = function(e) {
     e = e || window.event;
     var charCode = (typeof e.which == "number") ? e.which : e.keyCode;
     if (charCode) {
         console.log("Character typed: " + String.fromCharCode(charCode));
     }
-};
+ok agora vai pq bb pyboy tá funfando
+
+};*/
+const {PythonShell} = require( 'python-shell')
+var scriptPath = '/home/turuga/Documents/Shared/Productive/Projects/VisualKeyloggger/logger.py'
+var options = {
+    pythonOptions: ['-u']
+}
+var pyScript = new PythonShell(scriptPath, options)
+pyScript.on('message',function(message){
+    console.log(message)
+})
+pyScript.end(function(err){
+    if (err == 'KeyboardInterrupt') {
+        console.log('aff mano bom dia')
+    }
+    console.log('Finished w no mistakes')
+})
+
+document.onkeypress = function(e){
+    if(e.key == '1'){
+        pyScript.childProcess.kill('SIGINT')
+    }
+}
--- logger.py
+++ logger.py
@@ -1,22 +1,11 @@
-from pynput import keyboard
+from pynput import keyboard as kb
 
 def on_press(key):
-    try:
-        print('alphanumeric key {0} pressed'.format(
-            key.char))
-    except AttributeError:
-        print('special key {0} pressed'.format(
-            key))
+    print (format(key))
 
-def on_release(key):
-    print('{0} released'.format(
-        key))
-    if key == keyboard.Key.esc:
-        # Stop listener
-        return False
-
-# Collect events until released
-with keyboard.Listener(
-        on_press=on_press,
-        on_release=on_release) as listener:
+print('living out of my function')
+with kb.Listener(on_press = on_press) as listener:
+    print('pyshell got me here')
     listener.join()
+
+# print('testing python-shell')
