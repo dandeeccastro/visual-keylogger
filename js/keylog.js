ba8abc113c0fdadc177b3fbea41436cf8398ff40
dandeeccastro <dundeepessoal@gmail.com>
2019-02-28 12:55:33 -0300

    Basic keyboard style linked to switch, progress w keylogger communication w css

--- js/keylog.js
+++ js/keylog.js
@@ -1,28 +1,23 @@
-/* Essa função detecta keypresses no documento. Como fazer ela funcionar para o sistema todo?
-document.onkeypress = function(e) {
-    e = e || window.event;
-    var charCode = (typeof e.which == "number") ? e.which : e.keyCode;
-    if (charCode) {
-        console.log("Character typed: " + String.fromCharCode(charCode));
-    }
-ok agora vai pq bb pyboy tá funfando
-
-};*/
 let {PythonShell} = require('python-shell')
-var scriptPath = '/home/dundee/Documents/VisualKeyLogger/logger.py'
+// TODO: Function to get logger.py location on system
+var scriptPath = '/home/turuga/Documents/Shared/Productive/Projects/VisualKeyloggger/logger.py'
 var options = { pythonOptions: ['-u'] }
 var pyScript = null
 // ---------------------------------------------------------------------
 function startKeylogger(){
 	pyScript = new PythonShell(scriptPath,options)
 	pyScript.on('message',function(message){
-		console.log(message)
-	})
-	pyScript.end(function(err){
-		if (err == 'KeyboardInterrupt') {
-			console.log('aff mano bom dia')
+		var keyCollection = document.getElementsByClassName('key')
+		for(let i = 0; i < keyCollection.length; i++){
+			// pyScript sends key as an array w single quotes and the key, this is a workaround for that
+			if( keyCollection[i].innerHTML == message[1]){ 
+				Object.assign(keyCollection[i].style,{display:"none"});	
+				// TODO: Fix bug that never returns function
+				setTimeout(resetter(keyCollection,i), 100)
+			} else {
+				console.log('searching')
+			}
 		}
-		console.log('Finished w no mistakes')
 	})
 }
 // ---------------------------------------------------------------------
@@ -30,12 +25,26 @@ function endKeylogger(){
 	if(pyScript != null){
 		pyScript.childProcess.kill('SIGINT')
 	}
+	pyScript.end(function(err){
+		if (err == 'KeyboardInterrupt') {
+			console.log('aff mano bom dia')
+		}
+		console.log('Finished w no mistakes')
+	})
+}
+// ------------------------------------------------------------------
+function resetter(collection, i){
+	Object.assign(collection[i].style,{display:"inline"});
 }
 // ------------------------------------------------------------------
 function switx(){
 	if (document.getElementById('keylogger-switch').checked){
 		startKeylogger()
+		document.getElementById('title-container').style.display = 'none'
+		document.getElementById('keyboard-container').style.display = 'flex'
 	} else {
 		endKeylogger()
+		document.getElementById('keyboard-container').style.display = 'none'
+		document.getElementById('title-container').style.display = 'inline'
 	}
 }
